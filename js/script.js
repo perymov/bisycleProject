8b18e236b7b10c1ec8eab3abf1dec439b2736bbd
perymov <perymov82@gmail.com>
2020-06-27 21:08:43 +0700

    сделал слайдер

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ $(document).ready(function () {
 		$('.icon__menu, .menu__body').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+
+	$('.slider__body').slick({
+		arrows: false,
+		dots: true,
+		adaptiveHeight: true,
+		accessibility: false,
+		slidesToShow: 1,
+
+	});
 });
